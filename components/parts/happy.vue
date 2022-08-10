160da4f02404032d967fdd589d114535a044bfb8
TA <appulegugure@gmail.com>
2022-08-11 06:53:14 +0900

    add happy

--- components/parts/happy.vue
+++ components/parts/happy.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-    <h1>りんたろう誕生日まで</h1>
+    <h1>りんたろう22歳誕生日まで</h1>
     <h2>{{count}}</h2>
     <h1>{{day}}:{{hour}}:{{min}}:{{second}}</h1>
-    <div v-if="count <= 0">happy birth day!!</div>
   </div>
 </template>
 <script setup>
@@ -13,14 +12,14 @@ const hour = ref('')
 const min = ref('')
 const second = ref('')
 let displayshow = false
-const happybirthday = new Date('2022-08-11T00:00:00');
+const happybirthday = new Date('2023-08-11T00:00:00');
 watchEffect(() => {day.value,hour.value,min.value,second.value})
 setInterval(() => {
     let et = ((happybirthday.getTime() - new Date().getTime()))
-    count.value = et
-    day.value =  (Math.trunc(et/(1000*60*60*24)) > 10)? Math.trunc(et/(1000*60*60*24)) : '0' + Math.trunc(et/(1000*60*60*24));
-    hour.value = (Math.trunc(et/(1000*60*60)) > 10)? Math.trunc(et/(1000*60*60)) : '0' + Math.trunc(et/(1000*60*60));
-    min.value = (Math.trunc(et/(1000*60)%60) > 10)? Math.trunc(et/(1000*60)%60) : '0' + Math.trunc(et/(1000*60)%60); 
-    second.value = (Math.trunc(et/(1000)%60%60) > 10)? Math.trunc(et/(1000)%60%60) : '0' + Math.trunc(et/(1000)%60%60);
+    count.value = Math.abs(et)
+    day.value =  (Math.abs(Math.trunc(et/(1000*60*60*24))) > 10)? Math.trunc(et/(1000*60*60*24)) : '0' + Math.trunc(et/(1000*60*60*24));
+    hour.value = (Math.abs(Math.trunc(et/(1000*60*60))) > 10)? Math.trunc(et/(1000*60*60)%24) : '0' + Math.trunc(et/(1000*60*60)%24);
+    min.value = (Math.abs(Math.trunc(et/(1000*60)%60)) > 10)? Math.trunc(et/(1000*60)%60) : '0' + Math.trunc(et/(1000*60)%60); 
+    second.value = (Math.abs(Math.trunc(et/(1000)%60%60)) > 10)? Math.trunc(et/(1000)%60%60) : '0' + Math.trunc(et/(1000)%60%60);
   },100)
 </script>
