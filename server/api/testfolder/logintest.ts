23a26e33e011de82c2d89674dab2f1de50f4847a
appulegugure <appulegugure@gmail.com>
2022-12-09 12:35:50 +0900

    cookie　変んこう前

--- server/api/testfolder/logintest.ts
+++ server/api/testfolder/logintest.ts
@@ -3,12 +3,12 @@
 export default defineEventHandler(async (event) => {
 
     const content = await $fetch(`https://pz3dnfwt2g.execute-api.us-west-2.amazonaws.com/test2/login`,{ 
-      
+        
         parseResponse: JSON.parse,
         method: 'POST', 
         body: {
-          "username":"admin",
-          "password":"admin"
+            "username":"admin",
+            "password":"admin"
         }
     })
 
